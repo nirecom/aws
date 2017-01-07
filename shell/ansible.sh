a7ac46549ec30cae39586410536ab23ec6538c52
Hideaki Nire <hideaki@nire.com>
2017-01-07 12:36:16 +0900

    updated ubuntu / macos support

--- shell/ansible.sh
+++ shell/ansible.sh
@@ -1,15 +1,19 @@
-if [ "$(uname)" = 'Darwin' ]; then
-	OS='Mac'
-elif [ -f /etc/lsb-release ]; then
-	sudo apt-get -y update
-else
+if [ -f /etc/redhat-release ]; then
 	sudo yum -y update
+	sudo yum install epel-release
+	sudo yum install ansible sshpass
+elif [ -f /etc/lsb-release ]; then
+	sudo apt-get install software-properties-common
+	sudo apt-add-repository ppa:ansible/ansible
+	sudo apt-get update
+	sudo apt-get install ansible
+elif [ "$(uname)" = 'Darwin' ]; then
+	OS='Mac'
+	sudo easy_install pip
+	sudo -H pip install ansible
+# Older installation
+#	curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+#	sudo -H python get-pip.py
+#	sudo pip install ansible
+#	rm get-pip.py
 fi
-#sudo apt-get -y install python-pip
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-sudo -H python get-pip.py
-# for older Ubuntu OS
-#sudo apt-get -y install python2.7-dev
-#sudo pip install --upgrade setuptools --user python
-sudo pip install ansible
-rm get-pip.py
