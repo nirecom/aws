25210dda366a2db778876ae5f37c7bd05802dccf
nirecom <hideaki@nire.com>
2017-01-08 04:10:15 +0900

    add my_user and its register public key

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,1 +1,4 @@
-ansible-playbook -i hosts main.yml -u ansible --private-key=~/.ssh/id_ed25519_ansible
+ssh-add ~/.ssh/id_ed25519_ansible
+ansible-playbook -i hosts main.yml -u ansible --private-key=~/.ssh/id_ed25519_ansible --extra-vars="@private.yml"
+# when private.yml is encrypted as vault
+#ansible-playbook -i hosts main.yml -u ansible --private-key=~/.ssh/id_ed25519_ansible --extra-vars="@private.yml" --ask-vault-pass
