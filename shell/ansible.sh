b0c580d2f762023e93c4793fc3f30c3f22704bfb
Hideaki Nire <hideaki@nire.com>
2017-01-07 11:59:59 +0900

    added initial support for CentOS

--- shell/ansible.sh
+++ shell/ansible.sh
@@ -1,7 +1,9 @@
 if [ "$(uname)" = 'Darwin' ]; then
 	OS='Mac'
-else
+elif [ -f /etc/lsb-release ]; then
 	sudo apt-get -y update
+else
+	sudo yum -y update
 fi
 #sudo apt-get -y install python-pip
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
@@ -10,3 +12,4 @@ sudo -H python get-pip.py
 #sudo apt-get -y install python2.7-dev
 #sudo pip install --upgrade setuptools --user python
 sudo pip install ansible
+rm get-pip.py
